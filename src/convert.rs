1b096c2f7f5b8707cdaf0b342069dfd95e863114
vikrkuma <42959425+vikrkuma@users.noreply.github.com>
2019-01-31 19:55:01 +0530

    Change the version number and test case for //-- @content (#7)

--- src/convert.rs
+++ src/convert.rs
@@ -648,6 +648,15 @@ mod tests {
             json!("<p>-- @content[]/faq[] $faq_item</p>\n"),
         );
 
+        t(
+            r#"
+                -- @ROOT ~md
+                //-- @content[]/faq[] $faq_item
+            "#,
+            &ctx,
+            json!("<p>/-- @content[]/faq[] $faq_item</p>\n")
+        );
+
         /*t(
             r#"
                 -- $array2
